2993e22b1d8725c71b8149bacae09a183de7377b
Lennart <lennart@menschlennart.de>
2021-10-21 12:21:33 +0200

    Added basic artist scroll view on home

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -8,28 +8,53 @@ class HomeView extends StatelessWidget {
   static String routeName = "home";
 
   HomeView({Key? key}) : super(key: key);
-  final List<Artist> artists = [];
+  final List<Artist> artists = HomeModel().getArtists();
 
   @override
   Widget build(BuildContext context) {
-    Widget artistAvatar(Artist artist) {
-      return CircleAvatar(
-        maxRadius: 30,
-        child: ClipOval(
-          child: Image.asset('assets/images/adele.webp'),
-        ),
-        backgroundColor: Colors.transparent,
+    Widget artistAvatarBox(Artist artist) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            padding: const EdgeInsets.all(10),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 4,
+                  color: Colors.black26,
+                ),
+                image: DecorationImage(
+                    scale: 1,
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/${artist.artistId}.webp')),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 120,
+            child: Text(
+              artist.name,
+              softWrap: true,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       );
     }
 
-    final artistListView = ListView.builder(
-      itemCount: artists.length,
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      padding: const EdgeInsets.all(24),
-      itemBuilder: (context, index) {
-        return artistAvatar(artists[index]);
-      },
+    final artistScrollView = SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          for (int i = 0; i < artists.length; i++) artistAvatarBox(artists[i])
+        ],
+      ),
     );
 
     return Scaffold(
@@ -38,20 +63,17 @@ class HomeView extends StatelessWidget {
         toolbarHeight: 20,
         elevation: 0,
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ListView(
-            shrinkWrap: true,
-            key: key,
-            physics: const NeverScrollableScrollPhysics(),
-            padding: const EdgeInsets.all(24),
-            children: <Widget>[
-              artistListView,
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text('test'),
+            artistScrollView,
+          ],
+        ),
       ),
       bottomNavigationBar: ChangeNotifierProvider(
         create: (_) => HomeModel(),
